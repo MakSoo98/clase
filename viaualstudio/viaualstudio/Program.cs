cd58fbae698d1822d4ce3c4b83ab556dae5822ff
MakSoo98 <marcosferrerhernandez@gmail.com>
2020-10-08 14:41:30 +0200

    smile face
    aqui he puesto caritas sonrientes

--- viaualstudio/viaualstudio/Program.cs
+++ viaualstudio/viaualstudio/Program.cs
@@ -6,7 +6,7 @@ namespace viaualstudio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(";)");
         }
     }
 }
