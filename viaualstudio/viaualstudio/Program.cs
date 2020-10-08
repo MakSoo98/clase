e77e5564f95387acff7dc49a3eb4ce5ca078485d
MakSoo98 <marcosferrerhernandez@gmail.com>
2020-10-08 14:40:09 +0200

    clase
    este es el primer commit de clase

---
+++ viaualstudio/viaualstudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace viaualstudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
